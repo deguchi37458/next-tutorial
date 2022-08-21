9f08e12860c53bf7befd7856cf842739b6a0dfbb
deguchi <deguchi37458@gmail.com>
2022-08-21 11:50:21 +0900

    create container

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,15 @@
+import Container from "components/container";
 import Hero from 'components/hero';
 
 export default function About() {
   return (
     <>
+    <Container>
       <Hero
         title = "About"
         subtitle = "about development activities"
       />
+    </Container>
     </>
   )
 }
