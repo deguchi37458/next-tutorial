f47f9b3c2a2b53542ae3412ded45d91dee14ac38
deguchi <deguchi37458@gmail.com>
2022-08-23 18:37:18 +0900

    update about img

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,9 @@ import Container from "components/container";
 import Hero from 'components/hero';
 import PostBody from "components/post-body";
 import Contact from "components/contact";
-import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column";
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column"
+import Image from 'next/image'
+import eyecatch from 'images/about.jpg'
 
 export default function About() {
   return (
@@ -12,6 +14,18 @@ export default function About() {
         title = "About"
         subtitle = "about development activities"
       />
+
+      <figure>
+        <Image 
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+        />
+      </figure>
+
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
