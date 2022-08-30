96ef17cff33b894742eabbe623ed2e55f1d2a5fc
deguchi <deguchi37458@gmail.com>
2022-08-30 14:47:04 +0900

    update schedule.js

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,11 +1,14 @@
+import { client } from 'lib/api'
+
 export default function Schedule() {
   return <h1>記事のタイトル</h1>
 }
 
 export async function getStaticProps() {
-  console.log('1')
-  console.log('2')
-  console.log('3')
+  const resPromise = client.get({
+    endpoint: 'blogs',
+  })
+  resPromise.then((res) => console.log(res)).catch((err) => console.log(err))
   return {
     props: {},
   }
